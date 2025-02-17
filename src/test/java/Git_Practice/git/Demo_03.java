04a585e1c6f488c7898de47ade52c1fb460294ea
nasro <nasro@Nasroddin>
2025-02-17 18:17:13 +0530

    Home Page Test Cases Write By Amit Tester

---
+++ src/test/java/Git_Practice/git/Demo_03.java
@@ -0,0 +1,12 @@
+package Git_Practice.git;
+
+public class Demo_03 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test Cases write By Amit Automation Tester");
+		
+		
+	}
+	
+}
