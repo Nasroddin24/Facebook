de744e8a8e0a8d538d6c02f4c213712655d69b08
nasro <nasro@Nasroddin>
2025-02-17 17:25:03 +0530

    BDD FrameWork is created

---
+++ src/test/java/Git_Practice/git/Demo_01.java
@@ -0,0 +1,13 @@
+package Git_Practice.git;
+
+public class Demo_01 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("BDD Framework is Created By Team Leader");
+		
+	}
+	
+	
+}
